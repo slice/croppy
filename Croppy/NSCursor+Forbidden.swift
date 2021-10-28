dd97b01c3c7cf3c1d959219cba0ba5b0b5bf206e
slice <tinyslices@gmail.com>
2021-10-28 02:44:46 -0700

    Document NSCursor extensions

--- Croppy/NSCursor+Forbidden.swift
+++ Croppy/NSCursor+Forbidden.swift
@@ -1,13 +1,17 @@
 import Cocoa
 
 enum ForbiddenCursor: String {
+  /// The "resize north-west, south-east" cursor.
   case resizeNWSE = "_windowResizeNorthWestSouthEastCursor"
+
+  /// The "resize north-east, south-west" cursor.
   case resizeNESW = "_windowResizeNorthEastSouthWestCursor"
 }
 
 extension NSCursor {
-  // FREE THE CURSORS, APPLE.
+  /// Returns a forbidden cursor.
   static func forbidden(_ selector: ForbiddenCursor) -> NSCursor {
+    // FREE THE CURSORS, APPLE.
     NSCursor.perform(Selector(selector.rawValue), with: nil).takeUnretainedValue() as! NSCursor
   }
 }
